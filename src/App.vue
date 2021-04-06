75ef40a9a83c2d3eee6cd7fa33541c96174b26c4
itsChris <cc@solvia.ch>
2021-04-06 09:25:47 +0200

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
-<Test />
+  <Test />
 </template>
 
 <script>
-import Test from './components/Test'
+import Test from "./components/Test";
 export default {
-  name: 'App',
-  components: { Test
-  }
-}
+  name: "App",
+  components: { Test },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style.css";
 </style>
